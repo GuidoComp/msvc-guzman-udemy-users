9461ce7c26515e27cc1beeb38f538f109258a725
Guido Agustín Compagno <guidocompagno@gmail.com>
2025-06-20 12:56:57 -0300

    add and config micrometer tracing, add logs in controllers

--- src/main/java/com/guido/guzman/msv/users/controllers/UserController.java
+++ src/main/java/com/guido/guzman/msv/users/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.guido.guzman.msv.users.controllers;
 import com.guido.guzman.msv.users.entities.User;
 import com.guido.guzman.msv.users.services.IUserService;
 import com.guido.guzman.msv.users.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
     private final IUserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -17,32 +20,38 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User user) {
+        logger.info("UserController::create: creating {}", user);
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        logger.info("UserController::update: updating {}", user);
         return userService.update(id, user).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getById(@PathVariable Long id) {
+        logger.info("UserController::getById: getting User with id {}", id);
         return userService.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getByUsername(@PathVariable String username) {
+        logger.info("UserController::getByUsername: login with username {}", username);
         return userService.findByUsername(username).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
     public ResponseEntity<Iterable<User>> getAll() {
+        logger.info("UserController::getAll getting all Users");
         return ResponseEntity.ok(userService.findAll());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        logger.info("UserController::delete: deleting User with id {}", id);
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
